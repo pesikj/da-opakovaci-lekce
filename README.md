6a1b4ac22d27492607b0c65e678a84986c83221f
Jiří Pešík <jiri.pesik@outlook.cz>
2024-05-05 14:52:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Opakovací lekce Digitální akademie
+
+* [Otázky a řešení kvízu](kviz.ipynb)
+* [Další příklady na opakování](priklady.ipynb)
---
+++ opakovani.py
@@ -0,0 +1,12 @@
+mesicni_vydaje = [
+    {"kategorie": "Elektřina", "množství": 350, "cena_za_jednotku": 5.50},
+    {"kategorie": "Voda", "množství": 30, "cena_za_jednotku": 75.00},
+    {"kategorie": "Internet", "množství": 1, "cena_za_jednotku": 500.00},
+    {"kategorie": "Plyn", "množství": 100, "cena_za_jednotku": 10.00},
+]
+celkem = 0
+for radek in mesicni_vydaje:
+    ________________________________________
+
+
+
